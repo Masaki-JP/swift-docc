8aca376dbc08fda65d3171a152ddc5e8b1b97ba7
Masaki-JP <masakidayo0508@gmail.com>
2024-08-01 01:07:51 +0900

    Initial Commit

---
+++ Swift-Docc/Swift_DoccApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_DoccApp.swift
+//  Swift-Docc
+//
+//  Created by Masaki Doi on 2024/08/01.
+//
+
+import SwiftUI
+
+@main
+struct Swift_DoccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
